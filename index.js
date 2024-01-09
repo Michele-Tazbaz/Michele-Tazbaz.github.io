14487c85e354f6ec47113e327d6cc0773f78330c
Tazzy2022 <micheletazbaz@gmail.com>
2024-01-09 15:23:41 -0800

    updated links

---
+++ index.js
@@ -0,0 +1,20 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const pathName = window.location.pathname;
+const pageName = pathName.split("/").pop();
+
+if (pageName === "./") {
+  document.querySelector("home").classList.add("activeLink");
+}
+if (pageName === "about") {
+  document.querySelector(".about").classList.add("activeLink");
+}
+if (pageName === "projects") {
+  document.querySelector(".projects").classList.add("activeLink");
+}
+
+toggleButton.addEventListener("click", (e) => {
+  console.log("click");
+  console.log("e", e.target);
+  navbarLinks.classList.toggle("active");
+});
