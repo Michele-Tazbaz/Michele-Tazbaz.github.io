2ede40d2eb67fcdb3edd813c1f5f8c5a768fd038
Tazzy2022 <micheletazbaz@gmail.com>
2024-01-10 16:32:53 -0800

    finished for now

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-const pathName = window.location.pathname;
-const pageName = pathName.split("/").pop();
+// const pathName = window.location.pathname;
+// const pageName = pathName.split("/").pop();
 
-console.log("pageName", pageName);
+// console.log("pageName", pageName);
 
-if (pageName === ".") {
-  document.querySelector("home").classList.add("activeLink");
-}
-if (pageName === "about") {
-  document.querySelector(".about").classList.add("activeLink");
-}
-if (pageName === "projects") {
-  document.querySelector(".projects").classList.add("activeLink");
-}
+// if (pageName === ".") {
+//   document.querySelector("home").classList.add("activeLink");
+// }
+// if (pageName === "about") {
+//   document.querySelector(".about").classList.add("activeLink");
+// }
+// if (pageName === "projects") {
+//   document.querySelector(".projects").classList.add("activeLink");
+// }
 
 toggleButton.addEventListener("click", (e) => {
   console.log("click");
